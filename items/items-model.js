6b4a792204584933e745e721e24784cf7f9ee0fc
Bryan Szendel <bryan.szendel@gmail.com>
2019-11-26 13:38:44 -0700

    GET to /api/items/:id returns a single specific item

--- items/items-model.js
+++ items/items-model.js
@@ -2,8 +2,28 @@ const db = require('../database/db-config.js');
 
 module.exports = {
   find,
+  findById,
+  add,
+  edit,
+  remove
 }
 
 function find() {
+  return db('items')
+}
+
+function findById(id) {
+  return db('items').where('id', id).first()
+}
+
+function add(item) {
+  return db('items')
+}
+
+function edit(id, item) {
+  return db('items')
+}
+
+function remove(id) {
   return db('items')
 }
--- items/items-router.js
+++ items/items-router.js
@@ -13,4 +13,16 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  Items.findById(id)
+    .then(item => {
+      res.status(200).json(item)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Error retrieving the item.'})
+      console.log(err)
+    })
+})
+
 module.exports = router;
